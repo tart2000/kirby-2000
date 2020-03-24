170c9b41082f70953626b4262d957d1b02abbc50
Arthur <tart2000design@gmail.com>
2020-03-24 11:42:42 +0100

    grid OK plus navigation between pages

--- site/templates/pic.php
+++ site/templates/pic.php
@@ -1,26 +1,45 @@
 <?php snippet('header') ?>
 
-<div class="container img-page mt-5 mb-5">
+<div class="container-fluid img-page mt-5 mb-5">
 
 	<div class="row">
+		<div class="col-md-1">
+			<?php if ($page->hasPrev()) : ?>
+				<a href="<?= $page->prev()->url() ?>">
+					Previous
+				</a>
+			<?php endif ?>
+		</div>
+		<div class="col-md-10">
 			<h1><?= $page->title() ?></h1>
 			<?php if ($cover = $page->cover()->toFile()) : ?>
 				<?php $thumb = $cover->thumb([
 					'width' => 2000,
 					'quality' => 80
 				]) ?>
-				<img class="" src="<?= $thumb->url() ?>" class="img-fluid">
+				<img src="<?= $thumb->url() ?>" class="img-fluid">
 			<?php endif ?>
+			<div class="meta">
+				<b>Year:</b> <?= $page->year() ?><br>
+
+				<b>City:</b> <?= $page->city() ?><br>
+
+				<b>Country:</b> <?= $page->country() ?><br>
+
+				<b>Credits:</b> <?= $page->credits() ?><br>
+			</div>	
+		</div>
+		<div class="col-md-1">
+			<?php if ($page->hasNext()) : ?>
+				<a href="<?= $page->next()->url() ?>">
+					Next
+				</a>
+			<?php endif ?>
+		</div>
 	</div><!-- end card -->
 
 
-	<b>Year:</b> <?= $page->year() ?><br>
-
-	<b>City:</b> <?= $page->city() ?><br>
-
-	<b>Country:</b> <?= $page->country() ?><br>
-
-	<b>Credits:</b> <?= $page->credits() ?><br>
+	
 </div>
 
 <?php snippet('footer') ?>
